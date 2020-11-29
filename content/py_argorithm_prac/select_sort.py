baacd2fa846113390f9da0795029fa5093719430
fzk27 <feng@littlelights.ai>
2020-11-29 23:16:07 +0800

    add comment

--- content/py_argorithm_prac/select_sort.py
+++ content/py_argorithm_prac/select_sort.py
@@ -1,6 +1,6 @@
 '''
 Date: 2020-11-29 21:42:42
-LastEditTime: 2020-11-29 23:10:53
+LastEditTime: 2020-11-29 23:16:02
 '''
 
 def select_sort(arr):
@@ -8,6 +8,7 @@ def select_sort(arr):
     for i in range(len_arr):
         min_index = i
         for j in range(i+1, len_arr):
+            # 这里可以使用个方法，让对比可以泛化，或者实现python自身的比较的魔法方法都可以使用这个排序
             if arr[j] < arr[min_index]:
                 min_index = j
         arr[i], arr[min_index] = arr[min_index], arr[i]
