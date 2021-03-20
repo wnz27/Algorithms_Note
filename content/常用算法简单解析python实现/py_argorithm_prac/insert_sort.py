a69ef72778d5e04ac2013d90440a2c2f1df37f87
fzk27 <feng@littlelights.ai>
2021-03-20 17:38:55 +0800

    udpaet insert sort

--- content/常用算法简单解析python实现/py_argorithm_prac/insert_sort.py
+++ content/常用算法简单解析python实现/py_argorithm_prac/insert_sort.py
@@ -18,8 +18,37 @@ def insert_sort(arr: List[int]):
     return arr
 
 
+def insert_sort_update(arr: List[int]):
+    for i in range(len(arr)):
+        j = i
+        curr_value = arr[i]
+        while j - 1 >= 0:
+            if curr_value < arr[j - 1]:  # 如果这个值小于前一位
+                arr[j] = arr[j - 1]  # 前一位往后移动
+            else:
+                arr[j] = curr_value
+                break  # 不跳出赋值会出问题，前面都给盖掉了
+            j -= 1
+    return arr
+
+
+def insert_sort_update2(arr: List[int]):
+    for i in range(len(arr)):
+        j = i
+        curr_value = arr[i]
+        while (j - 1 >= 0) and (curr_value < arr[j - 1]):
+            arr[j] = arr[j - 1]  # 前一位往后移动
+            j -= 1
+        arr[j] = curr_value
+    return arr
+
+
 if __name__ == '__main__':
     a = [1, 2, -30, 399, 46, -3454, -40, 200]
     s_a = insert_sort(a)
     print(s_a)
+    s_a_up = insert_sort_update(a)
+    print(s_a_up)
+    s_a_up2 = insert_sort_update2(a)
+    print(s_a_up2)
     pass
